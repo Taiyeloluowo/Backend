4dbd7c825b6d5ebe5e913cfc08ac97936cbe78bb
Taiwo Owoseje <taiwoowoseje@gmail.com>
2025-10-17 16:15:00 +0100

    Week 11 Day 5 Backend update

--- Backend TUT/app.py
+++ Backend TUT/app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import uvicorn
 import os
 
+
 load_dotenv()
 
 app = FastAPI(title="Simple FastAPI App", version="1.0.0")
@@ -16,3 +17,28 @@ if __name__ == "__main__":
     print(os.getenv("host"))
     print(os.getenv("port"))
     uvicorn.run(app, host=os.getenv("host"), port=int(os.getenv("port")))
+
+class Item(BaseModel):
+    name: str = Field(..., example="Pepetual")
+    age: int = Field(..., example=25)
+    track: str = Field(..., example="Fullstack Developer")
+
+@app.get("/", description="This endpont just returns a welcome message") 
+def root():
+    return {"Message": "Welcome to my FastAPI Application"}
+
+@app.get("/get-data")
+def get_data():
+    return data
+
+@app.post("/create-data")
+def create_data(req: Item):
+    data.append(req.dict())
+    print(data)
+    return {"Message": "Data Received", "Data": data}
+
+@app.put("/update-data/{id}")
+def update_data(id: int, req: Item):
+    data[id = req.dict()]
+    print(data)
+    return {"Message": "Data Received", "Data": data}
--- Backend TUT/patch.py
+++ Backend TUT/patch.py
@@ -6,9 +6,6 @@ data = [
         "name": "Sam Larry",
         "track": "AI Developer"
     },
-    {
-        "name": "John Doe",
-    }   "track": "Backend Engineer"
 ]
 
 class BasicAPI(BaseHTTPRequestHandler):
@@ -20,13 +17,13 @@ class BasicAPI(BaseHTTPRequestHandler):
 
     def do_PATCH(self):
         content_size = int(self.headers.get("Content-Length", 0))
-        parsed_data = self.rfile.read(content_size)
+        raw_data = self.rfile.read(content_size)
 
-        patched_data = json.loads(parsed_data)
+        patched_data = json.loads(raw_data)
         if data:
             data[0].update(patched_data)
             self.send_data({
-                "Message": "Data Edited",
+                "Message": "Data updated successfully",
                 "data": data[0]
             }, status=201)
 
@@ -41,12 +38,43 @@ def run():
 print("Application is running")
 run()
 
-#  looking for the data
-        for item in data:
-            if item["id"]== id:
-                 item.update(data to update)
 
-                 if save_to_db(data):
-                      return self.send_data({"message":"Record updated", "data": item}, status=200)
-                 else:
-                      return self.send_data({"message":"Failed to save data"},status=400)
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+data = [
+    {
+        "name": "Ibrahim Lukman",
+        "track": "AI Developer"
+    }
+]
+
+class BasicAPI(BaseHTTPRequestHandler):
+    def send_data(self, data, status = 201):
+        self.send_response(status)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+
+    def do_PATCH(self):
+        content_size = int(self.headers.get("Content-Length",0))
+        parsed_data = self.rfile.read(content_size)
+
+        patched_data = json.loads(parsed_data)
+        if data:
+            data[0].update(patched_data)
+            self.send_data({
+                "Message": "Data Edited",
+                "data": data[0]
+            }, status=201)
+        
+        else:
+            self.send_data({
+                "Message": "No data to patch"
+            }, status=400)
+
+def run():
+    HTTPServer(('0.0.0.0', 8000), BasicAPI).serve_forever()
+
+print("Application is running")
+run()
